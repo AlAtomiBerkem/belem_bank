5ae9461fe1fff4f8492a4891cbc45ef3c98d3cfb
juneclyd <www.ramil241003@gmail.com>
2025-06-10 15:27:02 +0300

    feat: lint and formatier code done

--- documentMethodical/documentMethodicalKeyboard.js
+++ documentMethodical/documentMethodicalKeyboard.js
@@ -1,70 +1,69 @@
-const buttons = document.querySelectorAll('.btn')
-const input = document.querySelector('.search')
-const containerBoard = document.querySelector('.containerOne')
+const buttons = document.querySelectorAll(".btn");
+const input = document.querySelector(".search");
+const containerBoard = document.querySelector(".containerOne");
 
-const delete_btn = document.querySelector('.delete')
-const shift_btn = document.querySelector('.shift')
-const space_btn = document.querySelector('.space')
-const esc_btn = document.querySelector('.esc');
+const delete_btn = document.querySelector(".delete");
+const shift_btn = document.querySelector(".shift");
+const space_btn = document.querySelector(".space");
+const esc_btn = document.querySelector(".esc");
 
+let chars = [];
 
-let chars = []
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    input.value += btn.innerText;
+    chars = input.value.split("");
+  });
+});
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        input.value += btn.innerText
-        chars = input.value.split('')
-    })
-})
+delete_btn.addEventListener("click", () => {
+  chars.pop();
+  input.value = chars.join("");
+});
 
-delete_btn.addEventListener('click', () => {
-    chars.pop()
-    input.value = chars.join('')
-})
+space_btn.addEventListener("click", () => {
+  chars.push(" ");
+  input.value = chars.join("");
+});
 
-space_btn.addEventListener('click', () => {
-    chars.push(' ')
-    input.value = chars.join('')
-})
+shift_btn.addEventListener("click", () => {
+  buttons.forEach((btn) => {
+    btn.classList.toggle("upper");
+  });
+});
 
-shift_btn.addEventListener('click', () => {
-    buttons.forEach(btn => {
-        btn.classList.toggle('upper')
-    })
-})
+esc_btn.addEventListener("click", (event) => {
+  // event.preventDefault();
+  // input.value = ""
+  const keyboard = document.querySelector(".keyboard");
+  const isOpen = parseInt(window.getComputedStyle(keyboard).bottom) < 0;
+  animateKeyboard(isOpen);
 
-esc_btn.addEventListener('click', event => {
-    // event.preventDefault();
-    // input.value = ""
-    const keyboard = document.querySelector(".keyboard");
-    const isOpen = parseInt(window.getComputedStyle(keyboard).bottom) < 0;
-    animateKeyboard(isOpen);
-
-    setTimeout(() => {
-        fonElement.style.display = 'none';
-        containerBoard.style.display = 'none'
-    },500)
-})
+  setTimeout(() => {
+    fonElement.style.display = "none";
+    containerBoard.style.display = "none";
+  }, 500);
+});
 
 function animateKeyboard(open) {
-    const keyboard = document.querySelector(".keyboard");
-    let start = open ? -88 : 440;
-    let end = open ? 440 : -88;
-    let duration = 500; // Время анимации в миллисекундах
-    let startTime = null;
+  const keyboard = document.querySelector(".keyboard");
+  let start = open ? -88 : 440;
+  let end = open ? 440 : -88;
+  let duration = 500; // Время анимации в миллисекундах
+  let startTime = null;
 
-    function step(currentTime) {
-        if (!startTime) startTime = currentTime;
-        let progress = (currentTime - startTime) / duration;
+  function step(currentTime) {
+    if (!startTime) startTime = currentTime;
+    let progress = (currentTime - startTime) / duration;
 
-        if (progress > 1) progress = 1;
+    if (progress > 1) progress = 1;
 
-        keyboard.style.bottom = start + (end - start) * progress + "px";
+    keyboard.style.bottom = start + (end - start) * progress + "px";
 
-        if (progress < 1) {
-            requestAnimationFrame(step);
-        }
+    if (progress < 1) {
+      requestAnimationFrame(step);
     }
+  }
 
-    requestAnimationFrame(step);
+  requestAnimationFrame(step);
 }
--- folderDocument/folderDocument.js
+++ folderDocument/folderDocument.js
@@ -1,112 +1,131 @@
-const data = JSON.parse(localStorage.getItem('documents'));
+const data = JSON.parse(localStorage.getItem("documents"));
 console.log(data.folderDocument);
-const folderBlock = document.querySelector('.documentContainer');
-const folderTitle = document.createElement('div');
-const container = document.querySelector('.container');
-const inputSearch = document.querySelector('.search');
-const cancelSearch = document.querySelector('.cancelSearch');
-const keyboardBtns = document.querySelectorAll('.btn');
-const deleteBtn = document.querySelector('.delete');
-const fonElement = document.querySelector('.fon');
+const folderBlock = document.querySelector(".documentContainer");
+const folderTitle = document.createElement("div");
+const container = document.querySelector(".container");
+const inputSearch = document.querySelector(".search");
+const cancelSearch = document.querySelector(".cancelSearch");
+const keyboardBtns = document.querySelectorAll(".btn");
+const deleteBtn = document.querySelector(".delete");
+const fonElement = document.querySelector(".fon");
 const fullTitle = [];
 let flagKeyboard = true;
-data.folderDocument.forEach(el => {
-    fullTitle.push(el)
-    el.documents.forEach(element => {
-        fullTitle.push(element)
-    })
-})
-console.log(fullTitle)
+data.folderDocument.forEach((el) => {
+  fullTitle.push(el);
+  el.documents.forEach((element) => {
+    fullTitle.push(element);
+  });
+});
+console.log(fullTitle);
 
 function getFile(word, stations) {
-    return stations.filter(s => {
-      const regex = new RegExp(word, 'gi');
-      return s.title.match(regex);
-    })
+  return stations.filter((s) => {
+    const regex = new RegExp(word, "gi");
+    return s.title.match(regex);
+  });
 }
 
-inputSearch.addEventListener('change', (event) => {
-    console.log(getFile(inputSearch.value, fullTitle));
-})
-keyboardBtns.forEach(button => {
-    button.addEventListener('click', (event) => {
-        flagKeyboard = false;
-        const init = document.querySelector('.init');
-        let child = init.lastElementChild;
-        while(child) {
-            init.removeChild(child);
-            child = init.lastElementChild;
-        }
-        init.insertAdjacentHTML('beforeend', `<div class='documentContainer' style="width: 100%;"></div>`)
-        const folderBlockCopy = document.querySelector('.documentContainer');
-        const dataFilter = getFile(inputSearch.value, fullTitle);
-        dataFilter.forEach(el => {
-            folderBlockCopy.insertAdjacentHTML('beforeend', `
-                <a id='${el.id}' class='documentFon' href="${el.source}">
-                    <img class="documentFonImg" id='${el.idFolderDocument === undefined ? el.id : el.idFolderDocument}' src="${el.img}">
-                    <span class='documentFonSpan' id='${el.idFolderDocument === undefined ? el.id : el.idFolderDocument}'>${el.title}</span>
-                </a>
-            `);
-        });
-        new SimpleBar(folderBlockCopy);
-    });
+inputSearch.addEventListener("change", (event) => {
+  console.log(getFile(inputSearch.value, fullTitle));
 });
-deleteBtn.addEventListener('click', event => {
-    
-    if(inputSearch.value !== "") {
-        flagKeyboard = false;
-        const init = document.querySelector('.init');
-        let child = init.lastElementChild;
-        while(child) {
-            init.removeChild(child);
-            child = init.lastElementChild;
-        }
-        init.insertAdjacentHTML('beforeend', `<div class='documentContainer' style="width: 100%;"></div>`)
-        const folderBlockCopy = document.querySelector('.documentContainer');
-        const dataFilter = getFile(inputSearch.value, fullTitle);
-        dataFilter.forEach(el => {
-            folderBlockCopy.insertAdjacentHTML('beforeend', `
+keyboardBtns.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    flagKeyboard = false;
+    const init = document.querySelector(".init");
+    let child = init.lastElementChild;
+    while (child) {
+      init.removeChild(child);
+      child = init.lastElementChild;
+    }
+    init.insertAdjacentHTML(
+      "beforeend",
+      `<div class='documentContainer' style="width: 100%;"></div>`,
+    );
+    const folderBlockCopy = document.querySelector(".documentContainer");
+    const dataFilter = getFile(inputSearch.value, fullTitle);
+    dataFilter.forEach((el) => {
+      folderBlockCopy.insertAdjacentHTML(
+        "beforeend",
+        `
                 <a id='${el.id}' class='documentFon' href="${el.source}">
                     <img class="documentFonImg" id='${el.idFolderDocument === undefined ? el.id : el.idFolderDocument}' src="${el.img}">
                     <span class='documentFonSpan' id='${el.idFolderDocument === undefined ? el.id : el.idFolderDocument}'>${el.title}</span>
                 </a>
-            `);
-        });
-        new SimpleBar(folderBlockCopy);
-    } else {
-        flagKeyboard = true;
-        const init = document.querySelector('.init');
-        let child = init.lastElementChild;
-        while(child) {
-            init.removeChild(child);
-            child = init.lastElementChild;
-        }
-        init.insertAdjacentHTML('beforeend', `<div class='documentContainer' style="width: 100%;"></div>`)
-        const folderBlockCopy = document.querySelector('.documentContainer');
-        const dataFilter = data.folderDocument;
-        dataFilter.forEach(el => {
-            folderBlockCopy.insertAdjacentHTML('beforeend', `
+            `,
+      );
+    });
+    new SimpleBar(folderBlockCopy);
+  });
+});
+deleteBtn.addEventListener("click", (event) => {
+  if (inputSearch.value !== "") {
+    flagKeyboard = false;
+    const init = document.querySelector(".init");
+    let child = init.lastElementChild;
+    while (child) {
+      init.removeChild(child);
+      child = init.lastElementChild;
+    }
+    init.insertAdjacentHTML(
+      "beforeend",
+      `<div class='documentContainer' style="width: 100%;"></div>`,
+    );
+    const folderBlockCopy = document.querySelector(".documentContainer");
+    const dataFilter = getFile(inputSearch.value, fullTitle);
+    dataFilter.forEach((el) => {
+      folderBlockCopy.insertAdjacentHTML(
+        "beforeend",
+        `
+                <a id='${el.id}' class='documentFon' href="${el.source}">
+                    <img class="documentFonImg" id='${el.idFolderDocument === undefined ? el.id : el.idFolderDocument}' src="${el.img}">
+                    <span class='documentFonSpan' id='${el.idFolderDocument === undefined ? el.id : el.idFolderDocument}'>${el.title}</span>
+                </a>
+            `,
+      );
+    });
+    new SimpleBar(folderBlockCopy);
+  } else {
+    flagKeyboard = true;
+    const init = document.querySelector(".init");
+    let child = init.lastElementChild;
+    while (child) {
+      init.removeChild(child);
+      child = init.lastElementChild;
+    }
+    init.insertAdjacentHTML(
+      "beforeend",
+      `<div class='documentContainer' style="width: 100%;"></div>`,
+    );
+    const folderBlockCopy = document.querySelector(".documentContainer");
+    const dataFilter = data.folderDocument;
+    dataFilter.forEach((el) => {
+      folderBlockCopy.insertAdjacentHTML(
+        "beforeend",
+        `
                 <a id='${el.id}' class='documentFon' href="${el.source}">
                     <img src="${el.img}">
                     <span>${el.title}</span>
                 </a>
-            `);
-        });
-        new SimpleBar(folderBlockCopy);
-    }
-})
+            `,
+      );
+    });
+    new SimpleBar(folderBlockCopy);
+  }
+});
 
-
-data.folderDocument.forEach(el => {
-    folderBlock.insertAdjacentHTML('beforeend', `
+data.folderDocument.forEach((el) => {
+  folderBlock.insertAdjacentHTML(
+    "beforeend",
+    `
         <a id='${el.id}' class='documentFon' href="${el.source}">
             <img src="${el.img}">
             <span>${el.title}</span>
         </a>
-    `);
+    `,
+  );
 });
 new SimpleBar(folderBlock);
-// 
+//
 
 // document.querySelectorAll('.documentFon').forEach((item, index) => {
 //     item.addEventListener('click', event => {
@@ -115,11 +134,10 @@ new SimpleBar(folderBlock);
 //     });
 // });
 
-
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.style.opacity = 1;
+  document.body.style.opacity = 1;
 });
-// document.querySelectorAll(".documentFon").forEach((link, index) => {   
+// document.querySelectorAll(".documentFon").forEach((link, index) => {
 //     link.addEventListener("click", event => {
 //         event.preventDefault();
 //         //document.body.style.opacity = 0;
@@ -132,109 +150,114 @@ document.addEventListener("DOMContentLoaded", () => {
 //         // }, 500);
 //     });
 // });
-document.querySelector(".init").addEventListener("click", event => {
-    let link = event.target.closest(".documentFon"); 
-    let linkTwo = event.target.closest(".documentFonSpan"); 
-    let linkThree = event.target.closest(".documentFonImg"); 
-    if (!link && !linkTwo && !linkThree) {
-        return
-    } else {
-        console.log('Клик был на одном из элементов');
-    }
-    event.preventDefault();
-    console.log(11)
-    if(flagKeyboard === true) {
-        console.log('хуй1111')
-        console.log(link)
-        console.log(link.id)
-        localStorage.setItem('documentFolder', JSON.stringify(data.folderDocument[link.id]))
-        localStorage.setItem('idDocumentFolder', JSON.stringify(link.id))
-        document.body.style.opacity = 0;
-    } else if(flagKeyboard === false && !linkThree) {
-        console.log(linkTwo)
-        console.log(linkTwo.id)
-        localStorage.setItem('documentFolder', JSON.stringify(data.folderDocument[linkTwo.id]))
-        localStorage.setItem('idDocumentFolder', JSON.stringify(link.id))
-        document.body.style.opacity = 0;
-    } else {
-        console.log(linkThree);
-        console.log(linkThree.id);
-        localStorage.setItem('documentFolder', JSON.stringify(data.folderDocument[linkThree.id]))
-        localStorage.setItem('idDocumentFolder', JSON.stringify(link.id))
-        document.body.style.opacity = 0;
-    }
+document.querySelector(".init").addEventListener("click", (event) => {
+  let link = event.target.closest(".documentFon");
+  let linkTwo = event.target.closest(".documentFonSpan");
+  let linkThree = event.target.closest(".documentFonImg");
+  if (!link && !linkTwo && !linkThree) {
+    return;
+  } else {
+    console.log("Клик был на одном из элементов");
+  }
+  event.preventDefault();
+  console.log(11);
+  if (flagKeyboard === true) {
+    console.log("хуй1111");
+    console.log(link);
+    console.log(link.id);
+    localStorage.setItem(
+      "documentFolder",
+      JSON.stringify(data.folderDocument[link.id]),
+    );
+    localStorage.setItem("idDocumentFolder", JSON.stringify(link.id));
+    document.body.style.opacity = 0;
+  } else if (flagKeyboard === false && !linkThree) {
+    console.log(linkTwo);
+    console.log(linkTwo.id);
+    localStorage.setItem(
+      "documentFolder",
+      JSON.stringify(data.folderDocument[linkTwo.id]),
+    );
+    localStorage.setItem("idDocumentFolder", JSON.stringify(link.id));
+    document.body.style.opacity = 0;
+  } else {
+    console.log(linkThree);
+    console.log(linkThree.id);
+    localStorage.setItem(
+      "documentFolder",
+      JSON.stringify(data.folderDocument[linkThree.id]),
+    );
+    localStorage.setItem("idDocumentFolder", JSON.stringify(link.id));
+    document.body.style.opacity = 0;
+  }
 
-    setTimeout(() => {
-        window.location.href = link.href; 
-    }, 500);
+  setTimeout(() => {
+    window.location.href = link.href;
+  }, 500);
 });
 
+document.querySelector(".exit").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.body.style.opacity = 0;
+  setTimeout(() => {
+    window.location.href = "../main/main.html";
+  }, 500);
+});
 
-document.querySelector('.exit').addEventListener('click', (event) => {
-    event.preventDefault();
-        document.body.style.opacity = 0;
-        setTimeout(() => {
-            window.location.href = '../main/main.html';
-    }, 500);
-})
-
-document.querySelector('.search').addEventListener('click', event => {
-    fonElement.style.display = 'block';
-    container.style.display = 'block';
-    const keyboard = document.querySelector(".keyboard");
-    const isOpen = parseInt(window.getComputedStyle(keyboard).bottom) < 0;
-    animateKeyboard(isOpen);
-})
+document.querySelector(".search").addEventListener("click", (event) => {
+  fonElement.style.display = "block";
+  container.style.display = "block";
+  const keyboard = document.querySelector(".keyboard");
+  const isOpen = parseInt(window.getComputedStyle(keyboard).bottom) < 0;
+  animateKeyboard(isOpen);
+});
 
 function animateKeyboard(open) {
-    const keyboard = document.querySelector(".keyboard");
-    let start = open ? -505 : 25;
-    let end = open ? 25 : -505;
-    let duration = 500; // Время анимации в миллисекундах
-    let startTime = null;
+  const keyboard = document.querySelector(".keyboard");
+  let start = open ? -505 : 25;
+  let end = open ? 25 : -505;
+  let duration = 500; // Время анимации в миллисекундах
+  let startTime = null;
 
-    function step(currentTime) {
-        if (!startTime) startTime = currentTime;
-        let progress = (currentTime - startTime) / duration;
+  function step(currentTime) {
+    if (!startTime) startTime = currentTime;
+    let progress = (currentTime - startTime) / duration;
 
-        if (progress > 1) progress = 1;
+    if (progress > 1) progress = 1;
 
-        keyboard.style.bottom = start + (end - start) * progress + "px";
+    keyboard.style.bottom = start + (end - start) * progress + "px";
 
-        if (progress < 1) {
-            requestAnimationFrame(step);
-        }
+    if (progress < 1) {
+      requestAnimationFrame(step);
     }
+  }
 
-    requestAnimationFrame(step);
+  requestAnimationFrame(step);
 }
 
-
-
-
-document.querySelector('.simplebar-scrollbar').addEventListener('click', e => {
-    console.log(1000)
-})
-
+document
+  .querySelector(".simplebar-scrollbar")
+  .addEventListener("click", (e) => {
+    console.log(1000);
+  });
 
 let inactivityTime = 180000; // 3 минуты (в миллисекундах)
 let timeout;
 
 function resetTimer() {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-        window.location.href = "../main/main.html";
-    }, inactivityTime);
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    window.location.href = "../main/main.html";
+  }, inactivityTime);
 }
 
 // Запуск таймера при загрузке страницы
 document.addEventListener("DOMContentLoaded", resetTimer);
 
 // Сброс таймера при взаимодействии пользователя (нажатия, движения, скроллы)
-["click", "mousemove", "keypress", "touchstart", "scroll"].forEach(event => {
-    console.log(2)
-    document.addEventListener(event, resetTimer);
+["click", "mousemove", "keypress", "touchstart", "scroll"].forEach((event) => {
+  console.log(2);
+  document.addEventListener(event, resetTimer);
 });
 
-document.addEventListener('contextmenu', event => event.preventDefault());
-
+document.addEventListener("contextmenu", (event) => event.preventDefault());
