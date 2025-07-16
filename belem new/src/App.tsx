8e708a42a154474ac0b7e84307468d197e02a24a
annapirz <fanworld991@gmail.com>
2025-07-16 05:37:57 +0300

    feat: loading done

--- belem new/src/App.tsx
+++ belem new/src/App.tsx
@@ -1,11 +1,13 @@
 import Greeting from './components/Greeting.jsx'
+import Loading from './components/loading.jsx'
+
 import './index.css'
 
 function App() {
 
   return (
     <>
-      <Greeting />
+      <Loading />
     </>
   )
 }
