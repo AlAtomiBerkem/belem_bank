a9894bda4321cd8b6cf9cb7dc430b6a261554657
annapirz <fanworld991@gmail.com>
2025-07-16 04:48:08 +0300

    feat: greetion done

--- belem new/src/components/Greeting.jsx
+++ belem new/src/components/Greeting.jsx
@@ -1,11 +1,16 @@
 export const Greeting = () => {
-
     return (
-        <>
-        <article>здарова заебал</article>
-        <section> </section>
-        </>
+        <main className="relative h-screen w-screen bg-[url('/greetingBackFon.png')] bg-cover bg-center bg-fixed">
+            <div className="absolute bottom-42 left-0 w-full flex justify-center gap-20">
+                <button className="bg-transparent p-0 inline-flex items-center justify-center">
+                    <img src="/btn_document.png" alt="Документ" className="h-20" />
+                </button>
+                <button className="bg-transparent p-0 inline-flex items-center justify-center">
+                    <img src="/btn_posobie.png" alt="Пособие" className="h-20" />
+                </button>
+            </div>
+        </main>
     )
 }
 
-export default Greeting; 
+export default Greeting;
