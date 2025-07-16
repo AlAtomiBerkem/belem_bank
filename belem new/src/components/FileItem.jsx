206fd17ad1db4ffbcea0906a044bf55abd6c8985
annapirz <fanworld991@gmail.com>
2025-07-16 12:00:10 +0300

    feat: add folder/file items done

--- belem new/src/components/FileItem.jsx
+++ belem new/src/components/FileItem.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { useMarquee } from '../helpers/useMarquee.js'
 
-export const FileItem = ({fileName = 'тут будет находится пробное название файлов '}) => {
+export const FileItem = ({fileName = 'тут есть текст, очень длинный текст, ну очень длинный текст и опять же длинный текст '}) => {
+
+  const [containerRef, textRef] = useMarquee()
+
   return (
     <div className='bg-black'>
       <section className="scale-[0.8] inline-flex items-center relative  ">
         <img src="/fileItem.png" alt={fileName} />
-            <span className="ml-19 -mt-2  absolute inset-0 font-semibold text-white text-[35px] flex items-center justify-start">{fileName}</span>
+        <div
+          className="marquee-fade overflow-hidden whitespace-nowrap max-w-[1024px] flex items-center ml-19 -mt-2 absolute inset-0"
+          ref={containerRef}
+        >
+          <span
+            className="font-semibold text-white text-[35px] inline-block"
+            ref={textRef}
+            style={{ willChange: 'transform' }}
+          >
+            {fileName}
+          </span>
+        </div>
       </section>
     </div>
   )
